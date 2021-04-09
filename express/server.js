d67a9fd312ea211d9eb7981a1016ab78d33837ef
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-04-09 10:35:21 -0700

    Test Update

--- express/server.js
+++ express/server.js
@@ -6,9 +6,15 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+app.get('/results', (req, res) => {
+  console.log("GET RESULTS")
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write('<h1>Sending Results</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
@@ -17,7 +23,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/results', (req, res) => res.sendFile(path.join(__dirname, '/similarityResults.json')))
+app.use('/results', (req, res) => res.sendFile(path.join(__dirname, './similarityResults.json')))
 
 module.exports = app;
 module.exports.handler = serverless(app);
