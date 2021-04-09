601bd73e9610f36c00405c9ca38c0ca1fa370ebc
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-04-09 10:39:10 -0700

    Test Update

--- express/server.js
+++ express/server.js
@@ -6,12 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-app.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-app.get('/results', (req, res) => {
+router.get('/', (req, res) => {
   console.log("GET RESULTS")
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write('<h1>Sending Results</h1>');
@@ -22,8 +17,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/results', (req, res) => res.sendFile(path.join(__dirname, './similarityResults.json')))
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, './similarityResults.json')))
 
 module.exports = app;
 module.exports.handler = serverless(app);
